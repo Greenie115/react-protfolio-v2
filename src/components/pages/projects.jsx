ef993459bf837d48b608ca7ac970b401e026f5f6
Greenie115 <dggreen545@gmail.com>
2024-01-15 17:11:03 +0000

    updated projects cards

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -1,25 +1,110 @@
 import React from "react";
-import { myProjects } from "../localProjects"
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography, Button  } from "@mui/material";
 
-function Projects(){
-    return(
-    <>
-        <Box sx={{
-        mt: 9,
-        p: 3,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-    }}>
-       {myProjects.map((project) => {
-        <Paper elevation={3}>
-            <Typography variant="h4">{project.name}</Typography>
-        </Paper>
-       })}
-    </Box>
-    </>
+const myProjects = [
+    {
+        "id": "1",
+        "name": "README Generator",
+        "URL": "https://www.youtube.com/watch?v=S72KH97pHwM",
+        "github": "https://github.com/Greenie115/readme-generator",
+        "IMG": "./src/assets/READMEGeneratorImg.png",
+        "description": "Here's a README Generator"
+    },
+    {
+        "id": "2",
+        "name": "README Generator",
+        "URL": "https://www.youtube.com/watch?v=S72KH97pHwM",
+        "github": "https://github.com/Greenie115/readme-generator",
+        "IMG": "./src/assets/READMEGeneratorImg.png",
+        "description": "Here's a README Generator"
+    },
+    {
+        "id": "3",
+        "name": "README Generator",
+        "URL": "https://www.youtube.com/watch?v=S72KH97pHwM",
+        "github": "https://github.com/Greenie115/readme-generator",
+        "IMG": "./src/assets/READMEGeneratorImg.png",
+        "description": "Here's a README Generator"
+    },
+    {
+        "id": "4",
+        "name": "README Generator",
+        "URL": "https://www.youtube.com/watch?v=S72KH97pHwM",
+        "github": "https://github.com/Greenie115/readme-generator",
+        "IMG": "./src/assets/READMEGeneratorImg.png",
+        "description": "Here's a README Generator"
+    },
+    {
+        "id": "5",
+        "name": "README Generator",
+        "URL": "https://www.youtube.com/watch?v=S72KH97pHwM",
+        "github": "https://github.com/Greenie115/readme-generator",
+        "IMG": "./src/assets/READMEGeneratorImg.png",
+        "description": "Here's a README Generator"
+    },
+    {
+        "id": "6",
+        "name": "README Generator",
+        "URL": "https://www.youtube.com/watch?v=S72KH97pHwM",
+        "github": "https://github.com/Greenie115/readme-generator",
+        "IMG": "./src/assets/READMEGeneratorImg.png",
+        "description": "Here's a README Generator"
+    }
+]
+
+function Projects() {
+    return (
+        <>
+            <Typography variant="h3"
+                sx={{
+                    mt: 9,
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                Projects
+            </Typography>
+            <Box sx={{
+                mt: 2,
+                p: 3,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <Box>
+                {myProjects.map((project) => {
+                    return (
+                        <Paper key={project.id} square={false} elevation={4} sx={{
+                            m: 2
+                             
+                        }}>
+                            <Typography variant="h5" sx={{
+                                m: 2
+                            }}>
+                                {project.name}
+                            </Typography>
+                            <Typography variant="h6" sx={{
+                                m: 2
+                            }}>
+                                {project.description}
+                            </Typography>
+                            <img src={project.IMG} alt="" sx={{
+                                heigh: 10
+                            }}/>
+                            <Button variant="contained" sx={{
+                                m: 2
+                            }}
+                            >Contained</Button>
+                        </Paper>
+                    )
+                })}
+                </Box>
+            </Box>
+        </>
     )
-}  
+}
 
 export default Projects; 
