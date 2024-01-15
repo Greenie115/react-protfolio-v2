63d2ef1ae18b472c4408d90d04c56820d43524ca
Greenie115 <dggreen545@gmail.com>
2024-01-15 15:55:28 +0000

    updating proects and mapping the array

---
+++ src/components/localProjects.js
@@ -0,0 +1,45 @@
+
+export const myProjects = [
+    {
+        "name": "README Generator",
+        "URL": "https://www.youtube.com/watch?v=S72KH97pHwM",
+        "github": "https://github.com/Greenie115/readme-generator",
+        "IMG": "./src/assets/READMEGeneratorImg.png",
+        "description": "Here's a README Generator"
+    },
+    {
+        "name": "",
+        "URL": "",
+        "github": "",
+        "IMG": "",
+        "description": ""
+    },
+    {
+        "name": "",
+        "URL": "",
+        "github": "",
+        "IMG": "",
+        "description": ""
+    },
+    {
+        "name": "",
+        "URL": "",
+        "github": "",
+        "IMG": "",
+        "description": ""
+    },
+    {
+        "name": "",
+        "URL": "",
+        "github": "",
+        "IMG": "",
+        "description": ""
+    },
+    {
+        "name": "",
+        "URL": "",
+        "github": "",
+        "IMG": "",
+        "description": ""
+    }
+]
--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { myProjects } from "../localProjects"
+import { Box, Paper, Typography } from "@mui/material";
 
 function Projects(){
     return(
     <>
-    <p>Here's a project</p>
-    <p>Here's a project</p>
-    <p>Here's a project</p>
-    </>)
+        <Box sx={{
+        mt: 9,
+        p: 3,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    }}>
+       {myProjects.map((project) => {
+        <Paper elevation={3}>
+            <Typography variant="h4">{project.name}</Typography>
+        </Paper>
+       })}
+    </Box>
+    </>
+    )
 }  
 
 export default Projects; 
