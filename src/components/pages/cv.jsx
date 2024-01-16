859e89376757cbfd20a728dbff3466762000c0bc
Greenie115 <dggreen545@gmail.com>
2024-01-16 08:04:45 +0000

    downloading CV and CV page structure added

--- src/components/pages/cv.jsx
+++ src/components/pages/cv.jsx
@@ -1,12 +1,36 @@
+import { Box, Button, Paper, Typography } from "@mui/material";
 import React from "react";
 
-function CV(){
-    return(
+function CV() {
+    return (
         <>
-            <p>
-                Ohhhhhhh loook here's my CV 💪💪💪
-            </p>
-         </>
+            <Box sx={{
+                my: 12,
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <Paper elevation={3} sx={{
+                    p: 3,
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column"
+                }}>
+                    <Typography variant="h3" sx={{
+                        textAlign: "center",
+                        pb: 2
+                    }}>
+                        Check out my CV
+                    </Typography>
+                    <img src="./src\assets\CVScreenshot.png" alt="cv-screenshot"></img>
+                    <Button variant="contained" sx={{
+                        mt: 2
+                    }} href="Daniel Green Business Development CV.pdf"
+                        target="_blank"
+                        download="Daniel Green Business Development CV.pdf"
+                    >Download</Button>
+                </Paper>
+            </Box>
+        </>
     )
 }
 
